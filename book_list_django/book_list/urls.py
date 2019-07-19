c4d55e2e4689329c9363bd3b8223217641641057
amirali <amirali.mirian@gmail.com>
2019-07-19 17:42:40 +0430

    added a list view

--- book_list_django/book_list/urls.py
+++ book_list_django/book_list/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:book_id>/detail', views.detail, name='detail'),
+    path('<int:book_id>/detail/', views.detail, name='detail'),
+    path('list/', views.view_list, name='list'),
 ]
--- book_list_django/book_list/views.py
+++ book_list_django/book_list/views.py
@@ -5,3 +5,10 @@ from .models import Book
 
 def detail(request, book_id):
     return HttpResponse(f"You're looking at book: {Book.objects.get(pk=book_id)}")
+
+
+def view_list(request):
+    output = ""
+    for i, b in enumerate(Book.objects.order_by('-book_name')):
+        output += f"{i+1}: {b} \n"
+    return HttpResponse(output)
