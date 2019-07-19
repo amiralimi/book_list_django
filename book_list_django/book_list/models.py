2b1acdfb423fecde163397d299d833acac5d48fd
amirali <amirali.mirian@gmail.com>
2019-07-19 17:10:18 +0430

    Book model has been added to book_list app and it has been migrated

--- book_list_django/book_list/models.py
+++ book_list_django/book_list/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=200)
+    pub_date = models.DateField('date published')
+    page_num = models.IntegerField()
+    first_print = models.BooleanField()
+
+    def __str__(self):
+        return f"name: {self.book_name} \ndate published: {self.pub_date} \nnumber of pages: {self.page_num} \nfirst " \
+            f"print: {self.first_print} "
