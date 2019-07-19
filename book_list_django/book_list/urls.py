cbeff09cc1a704fdd0a016c97090654997ff1fc5
amirali <amirali.mirian@gmail.com>
2019-07-20 01:23:49 +0430

    added namespace to urls and url in list.html is now by namespace

--- book_list_django/book_list/urls.py
+++ book_list_django/book_list/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'book_list'
 urlpatterns = [
     path('<int:book_id>/detail/', views.detail, name='detail'),
     path('list/', views.view, name='list'),
